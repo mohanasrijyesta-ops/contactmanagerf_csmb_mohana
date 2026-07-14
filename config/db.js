c61e356c56de734044fef06a0c35833db9d925b1
mohanasrijyesta-ops <mohanasri.jyesta@gmail.com>
2026-07-14 14:22:56 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dns = require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://adariadithya816_db_user:kzsMeiCBrVfg2dUR@cluster0.qfimjmp.mongodb.net/Contactmanager?appName=Cluster0");
+        await mongoose.connect("mongodb+srv://mohanasrijyesta_db_user:2WVXT6C16t0s05V1@cluster0.ramksyt.mongodb.net/");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
