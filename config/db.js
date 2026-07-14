db985ca61021dc28e9313d56a5843b1dd70d0a4d
mohanasrijyesta-ops <mohanasri.jyesta@gmail.com>
2026-07-14 16:07:24 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dns = require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://mohanasrijyesta_db_user:2WVXT6C16t0s05V1@cluster0.ramksyt.mongodb.net/");
+        await mongoose.connect("mongodb+srv://mohanasrijyesta_db_user:2WVXT6C16t0s05V1@cluster0.ramksyt.mongodb.net/contact");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
